518a67311d6261c6ee5680b291ec552296ed4e67
PonzuFujikawa <fujikawa.ponzu.develop@gmail.com>
2020-10-29 00:04:46 +0900

    delete: middleware contentful getposts.ts

--- middleware/contentful/getPosts.ts
+++
@@ -1,8 +0,0 @@
-import { Context, Middleware } from "@nuxt/types"
-
-const getPosts: Middleware = async ({ store, payload }: Context,) => {
-  if (!store.state.posts.length && !payload) {
-    await store.dispatch('getPosts')
-  }
-}
-export default getPosts
