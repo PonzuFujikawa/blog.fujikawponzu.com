2d0074470a77fc62b456cc8cf40a99698941a3f5
PonzuFujikawa <fujikawa.ponzu.develop@gmail.com>
2020-11-21 21:53:41 +0900

    improve: dateformat

--- plugins/utils.ts
+++ plugins/utils.ts
@@ -2,7 +2,7 @@ import { Inject, NuxtApp } from "@nuxt/types/app"
 
 const getFormattedDate = (date: string) => {
   const originDate = new Date(date)
-  return `${originDate.getFullYear()}年${originDate.getMonth() + 1}月${originDate.getDate()}日`
+  return `${originDate.getFullYear()}/${originDate.getMonth() + 1}/${originDate.getDate()}`
 }
 
 export default ({}: NuxtApp, inject: Inject) => {
